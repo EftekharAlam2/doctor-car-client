0ca00a080444f3f11d3d871ceee4d8f14eec8db4
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-09 23:28:36 +0600

    Footer added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Footer from "../SharedPage/Footer";
 import NavBar from "../SharedPage/Navbar";
 
 const Home = () => {
   return (
     <div>
       <NavBar></NavBar>
+      <Footer></Footer>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div className="mx-w-7xl mx-auto">
+  <div className="max-w-7xl mx-auto">
     <React.StrictMode>
       <RouterProvider router={router} />
     </React.StrictMode>
