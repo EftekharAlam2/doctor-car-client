994fea585fdc513fd6545f1896d0e94ecdaa9065
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-09 23:11:10 +0600

    Navbar added

---
+++ src/Home/Home.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../SharedPage/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+    </div>
+  );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <div className="mx-w-7xl mx-auto">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </div>
 );
