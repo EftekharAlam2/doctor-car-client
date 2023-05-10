bc3d0356397200f0af1020e34b4556f3e00d1a1c
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-10 06:09:46 +0600

    banner section added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Footer from "../SharedPage/Footer";
 import NavBar from "../SharedPage/Navbar";
+import Banner from "./Pages/Banner";
 
 const Home = () => {
   return (
     <div>
       <NavBar></NavBar>
+      <Banner></Banner>
       <Footer></Footer>
     </div>
   );
