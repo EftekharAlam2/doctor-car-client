32e1c89f011206963b2b8af4af5aad501f2d22ce
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-10 06:57:54 +0600

    Service section added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Footer from "../SharedPage/Footer";
 import About from "./Pages/About";
 import Banner from "./Pages/Banner";
+import Services from "./Pages/Services";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <About></About>
+      <Services></Services>
       <Footer></Footer>
     </div>
   );
