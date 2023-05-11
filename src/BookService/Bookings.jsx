2ad1c16c6256bd87296204527bf2cc5d9bbe5060
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-11 19:15:50 +0600

    delete option added

--- src/BookService/Bookings.jsx
+++ src/BookService/Bookings.jsx
@@ -12,6 +12,24 @@ const Bookings = () => {
       .then((data) => setBookings(data));
   }, []);
 
+  const handleDelete = (id) => {
+    const proceed = confirm("Are You sure you want to delete");
+    if (proceed) {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("deleted successful");
+            const remaining = bookings.filter((booking) => booking._id !== id);
+            setBookings(remaining);
+          }
+        });
+    }
+  };
+
   return (
     <div>
       <div className="overflow-x-auto w-full">
@@ -36,7 +54,7 @@ const Bookings = () => {
               <BookingRow
                 key={booking._id}
                 booking={booking}
-                // handleDelete={handleDelete}
+                handleDelete={handleDelete}
                 // handleBookingConfirm={handleBookingConfirm}
               ></BookingRow>
             ))}
